0272be357033d98a279f5c7750f684186db1770f
AmeerSaadi <ameersaadi77@gmail.com>
2025-03-12 16:32:45 +0200

    Update E_RoomClient.ino
    הגדרת משתנים למעקב אחרי חידות הקשורות לאור ולחום כולל משתנים לעקוב אחרי זמן התחלת חידות רמות אור מקסימליות טמפרטורות נכונות זמן מדידה ומעקב אחרי פתרון החידה

--- sketch_mar12b/E_RoomClient.ino
+++ sketch_mar12b/E_RoomClient.ino
@@ -31,3 +31,16 @@ void SendGameOn();
 int currentPuzzle = 0;
 unsigned long currentMillis = 0;
 unsigned long lastSensorReadMillis = 0;
+
+unsigned long lightCorrectStartTime = 0;
+int maxLightLevel = 0;
+bool inCorrectLightRange = false;
+bool samplingMaxLight = true;
+unsigned long samplingStartTime = 0;
+
+float currentTemp = 0;
+float targetTemp = 0;
+bool tempPuzzleSolved = false;
+bool tempInCorrectRange = false;
+unsigned long tempCorrectStartTime = 0;
+static unsigned long lastTempCheckMillis = 0;
